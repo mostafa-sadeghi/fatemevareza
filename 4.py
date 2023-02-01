885fd2f346b5ed47b931d74dd5cc3275d0be4f57
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-01 21:59:46 +0330

    sadeghi

--- 4.py
+++ 4.py
@@ -1,26 +1,26 @@
-import turtle
+# import turtle
 
-s = turtle.Screen()
-s.bgcolor('orange')
-# s.bgpic('mario.png')
+# s = turtle.Screen()
+# s.bgcolor('orange')
+# # s.bgpic('mario.png')
 
-p = turtle.Pen()
-p.shape('turtle')
-p.shapesize(3)
-p.pensize(4)
-p.pencolor('red')
+# p = turtle.Pen()
+# p.shape('turtle')
+# p.shapesize(3)
+# p.pensize(4)
+# p.pencolor('red')
 
-p.fillcolor('green')
-p.begin_fill()
-p.forward(100)
-p.left(120)
-p.forward(100)
-p.left(120)
-p.forward(100)
-p.end_fill()
+# p.fillcolor('green')
+# p.begin_fill()
+# p.forward(100)
+# p.left(120)
+# p.forward(100)
+# p.left(120)
+# p.forward(100)
+# p.end_fill()
 
-p.hideturtle()
-s.exitonclick()
+# p.hideturtle()
+# s.exitonclick()
 
 # exercise 1
 '''
@@ -29,3 +29,22 @@ s.exitonclick()
 کشیدن پنج ضلعی توپر بارنگ قرمز
 
 '''
+import turtle
+s = turtle.Screen()
+s.bgcolor('yellow')
+p = turtle.Pen()
+p.color('red')
+p.pensize(4)
+p.home()
+p.fillcolor('blue')
+p.begin_fill()
+p.fd(100)
+p.left(90)
+p.fd(100)
+p.left(90)
+p.fd(100)
+p.left(90)
+p.fd(100)
+p.left(90)
+p.end_fill()
+s.exitonclick()
