ef808d9e0d8d51b3e26260d73cc68b6960f645ef
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-24 11:00:53 +0330

    sadeghi

---
+++ sample10.py
@@ -0,0 +1,46 @@
+score = 0
+#print("score is:", score)
+
+#score = score + 1
+#print("new score is:", score)
+# print()
+#score += 1
+# print("new score is:", score)
+# score += 2
+# print("score is:", score)
+# score = score - 1
+# print("new score is:", score)
+# score -= 1
+# print("new score is:", score)
+# +    -      *      /      %
+
+# print(4 % 2)
+# print(5 % 2)
+
+#print(4 / 2)
+# print(3 / 2)
+# print(3 // 2)
+
+# x = 2 + 3 * 2
+# print("x =", x)
+
+# x = (2 + 3) * 2
+# print("x =", x)
+
+# x = 2 + 2 / 2
+# print("x =", x)
+
+# x = 2 * 2 / 2
+# print("x =", x)
+
+# number_1 = int(input('enter number one: '))
+# number_2 = int(input('enter number two: '))
+
+# print("sum is:", number_1 + number_2)
+
+# برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید و
+# عدد اول و دوم را با هم جمع نماید و حاصل جمع را در عدد سوم ضرب کند
+# حاصل را پرینت نمائید.
+# عدد اعشاری
+number_1 = float(input('enter a number: '))
+print(number_1)
